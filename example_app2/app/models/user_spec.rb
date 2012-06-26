36fffede042cd0559239400561504076d06a2c0b
nguyenthai <forever.thai90@gmail.com>
2012-06-26 10:45:23 +0700

    Finish user edit,update,index,and destroy actions

--- example_app2/app/models/user_spec.rb
+++ example_app2/app/models/user_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe User do
 
+ 
+
   before { @user = User.new(name: "Example User", email: "user@example.com") }
 
   subject { @user }
-
+  
   it { should respond_to(:name) }
   it { should respond_to(:email) }
    private
--- example_app2/spec/models/user_spec.rb
+++ example_app2/spec/models/user_spec.rb
@@ -8,15 +8,24 @@ describe User do
   end
 
   subject { @user }
-
+  
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
-  it { should respond_to(:authenticate) }
   
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+
+    it { should be_admin }
+  end
+
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
